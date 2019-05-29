ea9f4b933e67e705dadbfc9f862f0a39a58ddc89
joshuareagan <joshuareagan@gmail.com>
2019-05-28 22:17:37 -0500

    corrections made in response to feedback

--- README.md
+++ README.md
@@ -1,37 +1,59 @@
 # Udacity SQL Project
 
-This program was written to complete the 'Logs Analysis' project for 
-Udacity's 'Full Stack Developer' course. It works by using the postgresql
-Python library to make three queries to a given database. The three results
-are then printed.
+## Project Description
 
-To run in the commandline, just do:
+This program was written to complete the 'Logs Analysis' project for 
+Udacity's 'Full Stack Developer' course. Given `newsdata.sql` (not 
+included in this repo), one generates the `news` database. Then 3
+queries are to be run on this database and the results displayed:
+
+1. The top 3 articles viewed.
+1. The top authors.
+1. All days in which the percentage of requests is more than 1% errors.
+
+## Dependencies
+
+The program uses postgresql, so you will need the `psycopg2` python 
+library. You also need the initial `newsdata.sql` file, not included 
+in this repo. (It's given as part of the assignment.)
+
+## Setup
+
+You can install the program by cloning the repo:
+
+```
+git clone https://github.com/joshuareagan/news.git
+```
+
+You'll also need to create the `news` database from `newsdata.sql` as
+follows:
+
+```
+psql -d news -f newsdata.sql
+```
+
+## Usage
+
+To run the program:
 
 ```
 python newsdata.py
 ```
 
-The 'news' database must be present.
-
 The output should look as follows:
 
 ```
 Top articles:
-"Candidate is jerk, alleges rival" -- 338647 views
-"Bears love berries, alleges bear" -- 253801 views
-"Bad things gone, say good people" -- 170098 views
-"Goats eat Google's lawn" -- 84906 views
-"Trouble for troubled troublemakers" -- 84810 views
-"Balloon goons doomed" -- 84557 views
-"There are a lot of bears" -- 84504 views
-"Media obsessed with bears" -- 84383 views
+  Candidate is jerk, alleges rival -- 338647 views
+  Bears love berries, alleges bear -- 253801 views
+  Bad things gone, say good people -- 170098 views
 -----
 Top authors:
-Ursula La Multa -- 507594 views
-Rudolf von Treppenwitz -- 423457 views
-Anonymous Contributor -- 170098 views
-Markoff Chaney -- 84557 views
+  Ursula La Multa -- 507594 views
+  Rudolf von Treppenwitz -- 423457 views
+  Anonymous Contributor -- 170098 views
+  Markoff Chaney -- 84557 views
 -----
 Days with more than 1% of requests being errors:
-July 17, 2016 -- 2.26%
+  July 17, 2016 -- 2.26%
 ```
--- newsdata.py
+++ newsdata.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import psycopg2
 
 dhand = psycopg2.connect("dbname=news")
@@ -6,32 +8,30 @@ curs = dhand.cursor()
 # The three SQL queries are kept in the strings
 # 'q_articles', 'q_authors', & 'q_errors'.
 
-# q_articles is for the top articles of all time.
-q_articles =  \
-    """
+# q_articles is for the top 3 articles of all time.
+q_articles = """
     select articles.title, count(*) as num from articles, log
-        where log.path like '%' || articles.slug
+        where substring(log.path from 10) = articles.slug
         group by articles.title
-        order by num desc;
+        order by num desc
+        limit 3;
     """
 
 # q_authors is for the most popular authors of all time.
-q_authors =  \
-    """
+q_authors = """
     select authors.name, count(*) as num from articles, log, authors
-        where log.path like '%' || articles.slug
+        where substring(log.path from 10) = articles.slug
         and articles.author = authors.id
         group by authors.name
         order by num desc;
     """
 
 # q_errors is for the days more than 1% of requests led to errors.
-q_errors =  \
-    """
+q_errors = """
     select a.date, errors, requests from
         (select date(time) as date, count(*) as errors from log
-            where status like '4%'
-            or status like '5%'
+            where substring(status for 1) = '4'
+            or substring(status for 1) = '5'
             group by date
         ) as a,
         (select date(time) as date, count(*) as requests from log
@@ -48,7 +48,7 @@ articles = curs.fetchall()
 
 print("Top articles:")
 for title, num in articles:
-    print('"'+title+'"'+' -- '+str(num)+' views')
+    print("  {} -- {} views".format(title, str(num)))
 print("-----")
 
 # Get and print the top authors.
@@ -58,7 +58,7 @@ authors = curs.fetchall()
 
 print("Top authors:")
 for author, num in authors:
-    print(author+' -- '+str(num)+' views')
+    print("  {} -- {} views".format(author, str(num)))
 print("-----")
 
 # Get and print days with > 1% errors.
@@ -70,6 +70,6 @@ print("Days with more than 1% of requests being errors:")
 for day, error, requests in errors:
     pretty_date = str(day.strftime("%B %d, %Y"))
     percent_error = round(error/float(requests)*100, 2)
-    print(pretty_date+' -- '+str(percent_error)+'%')
+    print("  {} -- {}%".format(pretty_date, str(percent_error)))
 
 curs.close()
